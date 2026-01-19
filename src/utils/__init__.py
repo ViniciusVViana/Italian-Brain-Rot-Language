6cc81c91d8d9b9513821355642bc2debfff3e8f5
ViniciusVViana <viniciusvianav1099@gmail.com>
2026-01-19 16:11:08 -0300

    correção das importações

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,2 @@
 from .lexical import lex
-from .syntactic import compute_first_follow, augment_grammar, build_slr_table, derv
+from .syntactic import compute_first_follow, augment_grammar, build_slr_table, derv, gramatica
