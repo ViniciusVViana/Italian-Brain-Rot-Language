086b7e82f4536c4e872366ddf2bc8456cb5c7f6f
ViniciusVViana <viniciusvianav1099@gmail.com>
2026-01-22 20:18:36 -0300

    removido verificação de declaração de variaveis

--- src/utils/lexical/ana_lex_util.py
+++ src/utils/lexical/ana_lex_util.py
@@ -12,9 +12,6 @@ def lex_anal(arch : str) -> list:
         for pattern, desc in patterns_list:
             match = re.match(pattern, arch)
             if match:
-                # verificar se a variavel ja foi declarada
-                if match.group(0) in [token[0] for token in tokens] and desc == "id" and tokens[-2][1] == "TIPO_DE_VARIAVEL":
-                    raise NameError(f"Erro léxico: Identificador '{match.group(0)}' já declarado na linha {line}.")
                 tokens.append((match.group(0), desc, line))
                 if tokens[-1][0] == "\n":
                     line += 1
