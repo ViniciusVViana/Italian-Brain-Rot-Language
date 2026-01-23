e97d40a351ba3c28bc6e009d254edb96e3bc72f8
ViniciusVViana <viniciusvianav1099@gmail.com>
2026-01-23 15:30:00 -0300

    removido a logica de escrita dos tokens em arquivo

--- src/utils/lexical/ana_lex_util.py
+++ src/utils/lexical/ana_lex_util.py
@@ -19,40 +19,23 @@ def lex_anal(arch : str) -> list:
                 break
     return tokens
 
-def lex(archive : str, flag : str ="c"):
+def lex(archive : str):
     
     with open(archive, "r", encoding="utf-8") as file:
         fl = file.read()
 
     tokens = lex_anal(fl)
-
-    if flag == "f":
-        archive_name = archive.split("\\")[-1].split(".")[0] + "_out.txt"
-        with open(f"saida/{archive_name}", "w", encoding="utf-8") as out_file:
-            if not tokens:
-                out_file.write("Nenhum token encontrado.\n")
-            else:
-                for token in tokens:
-                    val = token[0]
-                    if val == "\n":
-                        val = "\\n"
-                    elif val == "\t":
-                        val = "\\t"
-                    elif val == " ":
-                        val = "' '"
-                    out_file.write(f"[ {val}  -  {token[1]}  -  Linha {token[2]} ]\n")
-        print(f"Tokens escritos em {archive_name} no diretório saida.")
         
-    else:
-        if tokens:
-            print("Tokens encontrados:")
-            for token in tokens:
-                val = token[0]
-                if val == "\n":
-                    val = "\\n"
-                elif val == "\t":
-                    val = "\\t"
-                elif val == " ":
-                    val = "' '"
-                print(f"[ \033[34m{val}\033[0m  -  \033[32m{token[1]}\033[0m  -  \033[35mLinha {token[2]}\033[0m ]")
+    if tokens:
+        print("Tokens encontrados:")
+        for token in tokens:
+            val = token[0]
+            if val == "\n":
+                val = "\\n"
+            elif val == "\t":
+                val = "\\t"
+            elif val == " ":
+                val = "' '"
+            print(f"[ \033[34m{val}\033[0m  -  \033[32m{token[1]}\033[0m  -  \033[35mLinha {token[2]}\033[0m ]")
+
     return tokens
