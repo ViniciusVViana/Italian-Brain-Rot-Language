50224748a07502a07c00f7e0a3e91b873d78f222
ViniciusVViana <viniciusvianav1099@gmail.com>
2026-01-23 15:29:18 -0300

    incluidos modulos novos ao pacote

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,3 @@
 from .lexical import lex
 from .syntactic import compute_first_follow, augment_grammar, build_slr_table, derv, gramatica
-from .semantic import analyze_semantic
+from .semantic import SemanticAnalyzer, TACGenerator, generate_asm_from_tac
